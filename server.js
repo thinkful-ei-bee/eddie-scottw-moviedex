cd2d74affa01261ad5b6ccc37887e91d2aa194d4
Scott Williams <scottcolemanwilliams@gmail.com>
2019-04-03 11:46:06 -0500

    added handleValidation

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 'use strict';
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const movies = require('./movies');
 
+
+
 const app = express();
 
 app.use(morgan('dev'));
+
+function handleValidation(req, res, next) {
+  const key = process.env.API_TOKEN;
+  const userKey = req.get('Authorization');
+
+  if (!userKey) {
+    return res.status(401).json({error: 'Unauthorized access, no API key'});
+  }
+  if (userKey !== `Bearer ${key}`) {
+    return res.status(401).json({error: 'Unauthorized access, incorrect API key'});
+  }
+
+  next();
+}
+
+app.use(handleValidation);
 
 function handleGetMovie(req, res) {
   res.send(movies);
