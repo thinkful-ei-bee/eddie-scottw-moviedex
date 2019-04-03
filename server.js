3f2a75a9543c4c5a61507834cbb2a54e1db4fd83
Scott Williams <scottcolemanwilliams@gmail.com>
2019-04-03 13:55:12 -0500

    finished moviedex

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const movies = require('./movies');
+const cors = require('cors');
+const helmet = require('helmet');
 
 
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 
 function handleValidation(req, res, next) {
   const key = process.env.API_TOKEN;
@@ -27,7 +31,31 @@ function handleValidation(req, res, next) {
 app.use(handleValidation);
 
 function handleGetMovie(req, res) {
-  res.send(movies);
+  const {genre, country, avg_vote}= req.query;
+
+  let results = movies;
+
+  if (genre !== undefined) {
+    results = results.filter(movie => 
+      movie.genre.toLowerCase().includes(genre.toLowerCase())
+    )
+  }
+  if (country !== undefined) {
+    results = results.filter(movie => 
+      movie.country.toLowerCase().includes(country.toLowerCase())
+    )
+  }
+  if (avg_vote !== undefined)  {
+    if (isNaN(avg_vote) || parseFloat(avg_vote)<0 || parseFloat(avg_vote)>10) {
+      return res.status(400).send('avg_vote must be a number between 0 and 10')
+    }
+    results = results.filter(movie => 
+      movie.avg_vote >= parseFloat(avg_vote)
+    )
+  }
+
+
+  res.send(results);
 }
 
 app.get('/movie', handleGetMovie);
